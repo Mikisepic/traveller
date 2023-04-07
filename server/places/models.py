5ad027a46e714543e0ebc86465d3f5f87f972589
Mikas Pupeikis <mikas.pupeikis@mif.stud.vu.lt>
2023-04-07 23:32:38 +0300

    Resolve "Location CRUD"

--- server/places/models.py
+++ server/places/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
-# Create your models here.
+import uuid
 
 class Place(models.Model):
-  title = models.CharField(max_length=100)
-  description = models.CharField(max_length=1000)
-  lat = models.DecimalField(max_digits=9, decimal_places=6)
-  lng = models.DecimalField(max_digits=9, decimal_places=6)
-  isBookmarked = models.BooleanField(False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    lng = models.DecimalField(max_digits=9, decimal_places=6)
+    isBookmarked = models.BooleanField(False)
+
+    def __str__(self):
+        return self.title
--- server/places/urls.py
+++ server/places/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.PlaceCreateAPIView.as_view()),
+    path('<uuid:pk>', views.PlaceDetailsAPIView.as_view()),
 ]
